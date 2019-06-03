88e80cdce222dc3dbd88fade31231f71ef240538
ziyun <ziyun@meituan.com>
2019-06-04 02:08:27 +0800

    feat: update stack && binarySearch

---
+++ binarySearch/441. Arranging Coins/arrangeCoins.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @return {number} 
+ * 未使用二分查找
+ */
+var arrangeCoins = function(n) {
+    let l = 1;
+    let max = 0;
+    while (true) {
+        n = n - l;
+        l++;
+        if (n < 0) {
+            break;
+        }
+        max++;
+    }
+    return max;
+};
+
+var arrangeCoins = function(n) {
+    let l = 1;
+    let max = 0;
+    while (true) {
+        n = n - l;
+        l++;
+        if (n < 0) {
+            break;
+        }
+        max++;
+    }
+    return max;
+};
